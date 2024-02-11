a695cffa97e4ba1127aace1ca9756e7bd91b1efb
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2024-02-11 10:21:58 +0530

    Added www files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	header("Refresh: 1");
+	$conn = new mysqli('localhost','usersecam','secam123','secam');
+	if ($conn->connect_error) {
+  		die("Connection failed: " . $conn->connect_error);
+	}else{
+		$sql = "SELECT ts,data FROM img";	
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+  			while($row = $result->fetch_assoc()) {
+				echo '<div class="caption"><h3><img src="data:image/jpeg;base64,'.base64_encode($row['data']).'"/></br>'. $row['ts']. '</h3></div>';
+  			}						
+		}
+		$conn->close();
+	}
+?>
