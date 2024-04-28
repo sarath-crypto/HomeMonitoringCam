3c6e2b5d9c34e5788423f6cd8f697337279f7bd4
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2024-04-28 16:03:04 +0530

    updated

--- c.sh
+++ c.sh
@@ -1,1 +1,1 @@
-g++ secam.cpp motiondetector.o scanner.o `pkg-config --cflags --libs opencv4` -llccv  -lmysqlcppconn -Wno-psabi -o secam
+g++ ecsysapp.cpp motiondetector.o scanner.o miniaudio.o `pkg-config --cflags --libs opencv4` -llccv  -lmysqlcppconn  -Wno-psabi  -ldl -lm -lpthread -latomic -o ecsysapp
