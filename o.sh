5d8dab742f2cb1d2cb33bc46d80b854918d34305
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2024-04-28 20:36:16 +0530

    Delete o.sh

--- o.sh
+++
@@ -1,3 +0,0 @@
-g++ -c motiondetector.cpp `pkg-config --cflags --libs opencv4` -llccv  -lmysqlcppconn -lsoundio  -Wno-psabi  -ldl -lm -lpthread -latomic
-g++ -c scanner.cpp        `pkg-config --cflags --libs opencv4` -llccv  -lmysqlcppconn -lsoundio  -Wno-psabi  -ldl -lm -lpthread -latomic
-g++ -c miniaudio.c 	  `pkg-config --cflags --libs opencv4` -llccv  -lmysqlcppconn -lsoundio  -Wno-psabi  -ldl -lm -lpthread -latomic
